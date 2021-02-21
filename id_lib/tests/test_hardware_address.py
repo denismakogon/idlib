dd2c5ad5cec9e7b36dd39bff2e4088179b0ccc01
Denis Makogon <d.makogon@riseapps.biz>
2021-02-21 16:40:27 +0200

    testing

---
+++ id_lib/tests/test_hardware_address.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2021 Denys Makogon. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# ip a | grep ether | awk '{print $2}'
+
+import os
+
+from id_lib import id
+
+
+def test_hardware_address_in_id():
+    idg = id.ObjectID()
+    new_id = idg.get()
+    hw_addr = (
+        os.popen("""ip a | grep ether | awk '{print $2}'""")
+        .read()
+        .replace(":", "")
+        .upper()
+    )
+
+    assert hw_addr in new_id
